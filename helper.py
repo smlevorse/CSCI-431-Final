d08b12aa27262551156fb1a00f59f292defd1d3d
smlevorse <smlevorse@msn.com>
2017-12-12 02:02:43 -0500

    Saves normalized images now

--- helper.py
+++ helper.py
@@ -1,4 +1,6 @@
 import numpy as np
+import cv2
+from sklearn.preprocessing import normalize
 
 def getBinaryfer13Data(filename):
     labelsTrain = []
@@ -8,14 +10,17 @@ def getBinaryfer13Data(filename):
     imagesPubVal = []
     imagesPriVal = []
     first = True
+    count = 0
     for line in open(filename, 'rU'):
         if first:
             first = False
         else:
+            if count % 1000 == 0:
+                print("Processing image " + str(count))
             row = line.split(',')
             y = emotionToMatrix(int(row[0]))
             image = row[1].split(' ')
-            reshaped = np.reshape(image, (48, 48, 1)).astype(float) / 255.0
+            reshaped = normalizeImage(np.reshape(image, (48, 48, 1)).astype(float)) / 255
             image_type = row[2]
             if image_type.find('Training') >= 0:
                 labelsTrain.append(y)
@@ -26,9 +31,57 @@ def getBinaryfer13Data(filename):
             else:
                 labelsPriVal.append(y)
                 imagesPriVal.append(reshaped)
+        count = count + 1
     return np.array(imagesTrain), np.array(imagesPubVal), np.array(imagesPriVal), np.array(labelsTrain), np.array(labelsPubVal), np.array(labelsPriVal)
 
 def emotionToMatrix(y):
     output = np.zeros(7)
     output[y] = 1
     return output
+
+'''
+Gets the standard deviation in a 7x7 neighborhood around the specified pixel in the provided image
+Code created with help from:
+https://stackoverflow.com/questions/44906530/how-to-find-the-gaussian-weighted-average-and-standard-deviation-of-a-structural
+'''
+def StdDev(I, meanPoint, point):
+    # Check for edges of image
+    ystart = point[1] - 3 if 0 < point[1] - 3 < I.shape[0] else 0
+    yend = point[1] + 3 + 1 if 0 < point[1] + 3 + 1 < I.shape[0] else I.shape[0] - 1
+
+    xstart = point[0] - 3 if 0 < point[0] - 3 < I.shape[1] else 0
+    xend = point[0] + 3 + 1 if 0 < point[0] + 3 + 1 < I.shape[1] else I.shape[1] - 1
+
+    patch = (I[ystart:yend, xstart:xend] - meanPoint) ** 2
+    total = np.sum(patch)
+    n = patch.size
+
+    return 1 if total == 0 or n == 0 else np.sqrt(total / float(n))
+
+'''
+Nomalizes an image by performing x' = (x-gaussian_average)/(neighborhood_std)
+Based on "Facial expression recognition with Convolutional Neural Networks:
+Coping with few data and the training sample order" by Lopes, Aguiar, De Souza, and 
+Oliveira-Santos. 
+Code created with help from:
+https://stackoverflow.com/questions/44906530/how-to-find-the-gaussian-weighted-average-and-standard-deviation-of-a-structural
+'''
+def normalizeImage(I):
+    # Run a gaussian blur on the entire image with a kernel of size 7
+    blur = cv2.GaussianBlur(I, (7, 7), 0, 0).astype(np.float)
+    normalized = np.ones(I.shape, dtype=np.float) * 127
+
+    for i in range(I.shape[1]):
+        for j in range(I.shape[0]):
+            # For each pixel, get x, gaussian_average, and stddev, to calculate x prime
+            x = I[j, i]
+            gauss = blur[j, i]
+            std = StdDev(I, gauss, [i, j])
+
+            xp = 127  # default x prime in the middle of the range
+            if std > 0:
+                xp = (x - gauss) / float(std)
+
+            xp = np.clip((xp * 127 / float(2.0)) + 127, 0, 255)  #clip value to byte sized
+            normalized[j, i] = xp
+    return normalized
--- homework4.py
+++ homework4.py
@@ -1,64 +1,111 @@
 import tensorflow as tf
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Convolution2D, MaxPool2D, Flatten, Activation, Dense
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Activation, Dense, Dropout
 
+import pickle
+import os
 
 from helper import getBinaryfer13Data, emotionToMatrix
 
 def constructModel():
     model = Sequential()
-    model.add(Convolution2D(
+
+    model.add(Conv2D(
         input_shape = (48, 48, 1),
-        filters = 16,
-        kernel_size = 5,
-        strides = (1, 1),
-        padding = 'valid',
-        data_format = 'channels_last',
-        dilation_rate = (1, 1),
-        activation = 'relu',
-        use_bias = True,
-        kernel_initializer = 'random_uniform'
+        filters = 28,
+        kernel_size = (5,5),
+        strides = (1, 1)
+        # padding = 'valid',
+        # data_format = 'channels_last',
+        # dilation_rate = (1, 1),
+        # activation = 'relu',
+        # use_bias = True,
+        # kernel_initializer = 'random_uniform'
     ))
-    model.add(MaxPool2D(pool_size = (2, 2)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size = (2, 2)))
     # Should result in 24x24x1 output
 
-    model.add(Convolution2D(
-        filters = 16,
-        kernel_size = 5,
-        activation = 'relu',
-        use_bias = True,
-        kernel_initializer = 'random_uniform',
-        bias_initializer = 'zeros',
+    model.add(Conv2D(
+        filters = 8,
+        kernel_size = (7, 7),
+        # activation = 'relu',
+        # use_bias = True,
+        # kernel_initializer = 'random_uniform',
+        # bias_initializer = 'zeros',
     ))
-    model.add(MaxPool2D(pool_size = (2, 2)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size = (2, 2)))
     # Should result in 12x12x1 output
 
     model.add(Flatten())
-    model.add(Dense(7))
+    model.add(Dense(144))
     model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(7))
+    model.add(Activation('sigmoid'))
+
     model.compile(loss = 'categorical_crossentropy', optimizer = 'sgd', metrics = ['accuracy'])
 
     return model
 
+'''
+Save time by not running the normalize function on each image each time we run the program
+'''
+def loadImages():
+    allFilesExist = True
+    allFilesExist = allFilesExist and os.path.exists('nomalizeTrainingImages') and os.path.exists('trainingLabels')
+    allFilesExist = allFilesExist and os.path.exists('normalizedPublicValidationImages') and os.path.exists('publicValidationLabels')
+    allFilesExist = allFilesExist and os.path.exists('normalizedPrivateValidationImages') and os.path.exists('privateValidationLabels')
+
+    if allFilesExist:
+        print("Normalized images found, unpickling...")
+        with open('nomalizeTrainingImages', 'rb') as fi:
+            imagesT = pickle.load(fi)
+        with open('trainingLabels', 'rb') as fi:
+            labelsT = pickle.load(fi)
+        with open('normalizedPublicValidationImages', 'rb') as fi:
+            imagesPubV = pickle.load(fi)
+        with open('publicValidationLabels', 'rb') as fi:
+            labelsPubV = pickle.load(fi)
+        with open('normalizedPrivateValidationImages', 'rb') as fi:
+            imagesPriV = pickle.load(fi)
+        with open('privateValidationLabels', 'rb') as fi:
+            labelsPriV = pickle.load(fi)
+    else:
+        print("No normalized images found, loading from CSV")
+        imagesT, imagesPubV, imagesPriV, labelsT, labelsPubV, labelsPriV = getBinaryfer13Data('fer2013.csv')
+        # Store normalized images in pickled files to save that ungodly normalizing time again
+        with open('nomalizeTrainingImages', 'wb') as fi:
+            pickle.dump(imagesT, fi)
+        with open('trainingLabels', 'wb') as fi:
+            pickle.dump(labelsT, fi)
+        with open('normalizedPublicValidationImages', 'wb') as fi:
+            pickle.dump(imagesPubV, fi)
+        with open('publicValidationLabels', 'wb') as fi:
+            pickle.dump(labelsPubV, fi)
+        with open('normalizedPrivateValidationImages', 'wb') as fi:
+            pickle.dump(imagesPriV, fi)
+        with open('privateValidationLabels', 'wb') as fi:
+            pickle.dump(labelsPriV, fi)
+
+    return imagesT, imagesPubV, imagesPriV , labelsT, labelsPubV, labelsPriV
+
 def main():
     print('loading images')
-    imagesT, imagesV, labelsT, labelsV = getBinaryfer13Data('fer2013.csv')
-
-    print(imagesT.shape)
-    print(labelsT.shape)
+    imagesT, imagesPubV, imagesPriV , labelsT, labelsPubV, labelsPriV = loadImages()
 
     print('done loading')
     print('constructing model')
     model = constructModel()
     print('model constructed')
     print('training model')
-    model.fit(imagesT, labelsT, epochs = 50, validation_data = (imagesV, labelsV))
+    model.fit(imagesT, labelsT, epochs = 10, validation_data = (imagesPubV, labelsPubV))
     print('model trained')
     print('saving weights')
     model.save_weights('weights.h5')
     print('saved!')
-
 
 if __name__ == "__main__":
     main()
